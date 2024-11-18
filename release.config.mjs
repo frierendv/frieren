b382cb4802087114ce472b21b4bef69426682674
FrierenDv <hackerxct007@gmail.com>
2024-11-19 03:44:56 +0700

    chore: add GitHub release asset configuration to semantic-release

--- release.config.mjs
+++ release.config.mjs
@@ -16,6 +16,12 @@ export default {
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
+		[
+			"@semantic-release/github",
+			{
+				assets: ["dist/**"],
+			},
+		],
 		"@semantic-release/git",
 	],
 };
