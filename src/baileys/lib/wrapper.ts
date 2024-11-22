3770722f761bf7fc68ecb02560131ca00cb447cc
FrierenDv <hackerxct007@gmail.com>
2024-11-23 06:19:30 +0700

    feat: enhance wrap function to support optional callback and return error

--- src/baileys/lib/wrapper.ts
+++ src/baileys/lib/wrapper.ts
@@ -1,7 +1,12 @@
-export const wrap = (fn: () => void, cb: (_error: unknown) => void): void => {
+type Callback<T = unknown> = (_error: T) => void;
+
+export const wrap = <T>(fn: () => T, cb?: Callback): T => {
 	try {
 		return fn();
 	} catch (error) {
-		cb(error);
+		if (cb && typeof cb === "function") {
+			cb(error);
+		}
+		return error as T;
 	}
 };
