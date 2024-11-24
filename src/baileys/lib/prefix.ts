08dcca05108db080d2db28156b81864686b60c2e
FrierenDv <hackerxct007@gmail.com>
2024-11-24 18:19:55 +0700

    refactor: update extractPrefix function to improve prefix extraction and add normalizePrefix utility

--- src/baileys/lib/prefix.ts
+++ src/baileys/lib/prefix.ts
@@ -1,26 +1,31 @@
-import { ICommand, IContextMessage } from "../types";
-
 export const extractPrefix = (
-	_prefix: string | string[] | null,
+	_prefix: string[],
 	text: string
-): ICommand & Pick<IContextMessage, "text" | "args"> => {
+): { prefix: string; command: string } => {
 	let command = "";
-	const prefix =
-		(Array.isArray(_prefix)
-			? _prefix.find((prefix) => text.startsWith(prefix))
-			: _prefix) ?? "";
-
+	const prefix = _prefix.find((prefix) => text.startsWith(prefix));
+	if (!prefix) {
+		return { prefix: "", command };
+	}
 	text = text.slice(prefix.length).trim();
 	const args = text.split(" ");
 
-	command = args.shift()?.toLowerCase() ?? "";
-	// remove prefix and command from text
-	text = text.replace(command, "").trim();
+	command = args.shift() ?? "";
 
 	return {
 		prefix,
 		command,
-		text,
-		args,
 	};
+};
+
+export const normalizePrefix = (
+	prefix?: string | string[] | null
+): string[] => {
+	if (!prefix) {
+		return [];
+	}
+	if (Array.isArray(prefix)) {
+		return prefix;
+	}
+	return [prefix];
 };
