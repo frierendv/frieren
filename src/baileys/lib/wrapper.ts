794dcb7991d2040a2a5a9a489572e4ab622f3b9e
FrierenDv <hackerxct007@gmail.com>
2024-11-24 18:18:45 +0700

    refactor: update wrap function to support asynchronous execution with await

--- src/baileys/lib/wrapper.ts
+++ src/baileys/lib/wrapper.ts
@@ -1,8 +1,8 @@
 type Callback<T = unknown> = (_error: T) => void;
 
-export const wrap = <T>(fn: () => T, cb?: Callback): T => {
+export const wrap = async <T>(fn: () => T, cb?: Callback): Promise<T> => {
 	try {
-		return fn();
+		return await fn();
 	} catch (error) {
 		if (cb && typeof cb === "function") {
 			cb(error);
