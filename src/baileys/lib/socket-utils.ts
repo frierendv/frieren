e37bb60f75fbb7bdc3d2125ef53d5111db7b7a27
FrierenDv <hackerxct007@gmail.com>
2024-11-29 04:23:44 +0700

    refactor: simplify file handling by replacing synchronous methods with asynchronous alternatives and improving type checks

--- src/baileys/lib/socket-utils.ts
+++ src/baileys/lib/socket-utils.ts
@@ -1,111 +1,93 @@
 import { AnyMediaMessageContent } from "baileys";
 import { fromBuffer } from "file-type";
-import {
-	existsSync,
-	readFileSync,
-	statSync,
-	unlinkSync,
-	writeFileSync,
-} from "fs";
-import os from "os";
+import { existsSync } from "fs";
+import { readFile } from "fs/promises";
 import { fetch } from "undici";
-import { MIMEType } from "util";
 import { SUPPORTED_MEDIA_TYPES } from "../resource/message";
-import { wrap } from "./wrapper";
-
-const getDefaultBuffer = () => Buffer.alloc(0);
-
-type AnyContent = string | Buffer | ArrayBuffer;
-
-const getTemporaryPath = (extension: string) =>
-	`${os.tmpdir()}/${Math.random().toString(36).substring(7)}.${extension}`;
-
-const createFile = async (path: string, buffer: Buffer) => {
-	writeFileSync(path, buffer);
-	return {
-		path,
-		unlink: () =>
-			wrap(
-				() => unlinkSync(path),
-				() => {}
-			),
-	};
-};
-
-// Type Guards
-const isBuffer = (content: AnyContent): content is Buffer | ArrayBuffer =>
-	content instanceof Buffer || content instanceof ArrayBuffer;
-
-const isDataURL = (str: string): boolean => /^data:/.test(str);
-
-const isLocalFile = (str: string): boolean =>
-	typeof str === "string" && existsSync(str);
-
-const isURL = (str: string): boolean =>
-	typeof str === "string" && /^https?:\/\//.test(str);
 
 export const downloadFile = async (content: AnyContent): Promise<Buffer> => {
 	if (isBuffer(content)) {
-		return Buffer.from(content);
+		return content as Buffer;
 	}
 	if (isDataURL(content as string)) {
-		const res = await fetch(content as string);
-		return Buffer.from(await res.arrayBuffer());
+		return Buffer.from(
+			(content as string).split("base64,").pop() ?? "",
+			"base64"
+		);
 	}
 	if (isLocalFile(content)) {
-		return readFileSync(content);
+		return await readFile(content as string);
 	}
-	if (isURL(content)) {
-		try {
-			const res = await fetch(content as string);
-			return Buffer.from(await res.arrayBuffer());
-		} catch {
-			return getDefaultBuffer();
-		}
+	const response = await fetch(content as string);
+
+	if (!response?.body) {
+		throw new Error("No body in response");
 	}
-	return getDefaultBuffer();
+
+	const chunks: Uint8Array[] = [];
+	for await (const chunk of response.body) {
+		chunks.push(chunk);
+	}
+
+	return Buffer.concat(chunks);
 };
 
 export const getSendFileOptions = async (
 	content: AnyContent,
-	fileName?: string,
+	filename?: string,
 	caption?: string
 ) => {
 	const buffer = await downloadFile(content);
 	const fileType = await fromBuffer(buffer);
-	const mime = new MIMEType(fileType?.mime ?? "application/octet-stream");
-	const mimeType = new MIMEType(fileType?.mime ?? "application/octet-stream");
 
-	type SupportedMediaTypes = (typeof SUPPORTED_MEDIA_TYPES)[number];
+	const type = getFileType(fileType);
+	const finalType = getFinalType(buffer.length, type);
+	const sendOptions = getSendOptions(fileType, filename, caption);
 
-	const type = SUPPORTED_MEDIA_TYPES.includes(
-		mimeType.type as SupportedMediaTypes
-	)
-		? mimeType.type
-		: "document";
-
-	const path = getTemporaryPath(mime.subtype ?? mime.type ?? "bin");
-	await createFile(path, buffer);
-	const size = statSync(path).size;
-
-	const finalType = size > 64 * 1024 * 1024 ? "document" : type;
-	const sendOptions = {
-		fileName: fileName || path.split("/").pop(),
-		mimetype: mime.type,
-		caption: /webp|audio/i.test(mime.toString()) ? undefined : caption,
-	};
-
-	if (/audio/i.test(mime.toString())) {
-		sendOptions.mimetype = "audio/ogg; codecs=opus";
-	}
 	return {
 		sendOptions: sendOptions as unknown as AnyMediaMessageContent,
-		path,
 		type: finalType,
-		unlink: () =>
-			wrap(
-				() => unlinkSync(path),
-				() => {}
-			),
+		buffer,
 	};
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getFileType = (fileType: any) => {
+	type SupportedMediaTypes = (typeof SUPPORTED_MEDIA_TYPES)[number];
+	return SUPPORTED_MEDIA_TYPES.includes(
+		fileType?.mime.split("/")[0] as SupportedMediaTypes
+	)
+		? fileType?.mime.split("/")[0]
+		: "document";
+};
+
+const getFinalType = (size: number, type: string) => {
+	return size > 64 * 1024 * 1024 ? "document" : type || "document";
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getSendOptions = (fileType: any, filename?: string, caption?: string) => {
+	const sendOptions = {
+		filename: filename ?? `file.${fileType?.ext ?? "bin"}`,
+		mimetype: fileType?.mime ?? "application/octet-stream",
+		caption: /webp|audio/i.test(fileType?.mime ?? "") ? caption : undefined,
+	};
+
+	if (/audio/i.test(fileType?.mime ?? "")) {
+		sendOptions.mimetype = "audio/ogg; codecs=opus";
+	}
+
+	return sendOptions;
+};
+
+export type AnyContent = string | Buffer | ArrayBuffer;
+
+export const isBuffer = (
+	content: AnyContent
+): content is Buffer | ArrayBuffer =>
+	content instanceof Buffer || content instanceof ArrayBuffer;
+
+export const isDataURL = (str: string): boolean => /^data:/.test(str);
+
+export const isLocalFile = (str: string): boolean =>
+	typeof str === "string" && existsSync(str);
